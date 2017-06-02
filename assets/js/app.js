875f37e43cc54d11b7bd3c0a6fcca184f12ac653
LeidyRosmery <rosm3rita@hotmail.com>
2017-06-02 17:18:30 -0500

    slider ciclico

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
+
   $('.after').on('click', function(){
     var imagenActual=$('.active');
     var imagenSiguiente=imagenActual.next();
-    console.log(imagenSiguiente);
+    var ultimoImagen=$('#slider img:last-child');
+    console.log(imagenActual);
+      console.log(ultimoImagen);
+
+  /*  if(imagenActual==ultimoImagen){
+      $('#slider img:first-child').insertBefore('#slider img:last-child');
+    }*/
     if(imagenSiguiente.length){
       imagenActual.removeClass('active').css('z-index',-10);
       imagenSiguiente.addClass('active').css('z-index',10);
-    }
+    $('#slider img:last-child').insertBefore('#slider img:first-child');
+      }
   });
   $('.before').on('click', function(){
     var imagenActual=$('.active');
@@ -16,6 +24,7 @@ $(document).ready(function(){
       console.log("ingressa al if");
       imagenActual.removeClass('active').css('z-index',-10);
       imagenAnterior.addClass('active').css('z-index',10);
+          $('#slider img:last-child').insertBefore('#slider img:first-child');
     }
   });
 });
