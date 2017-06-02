a20dcbd40ae4917b2a80097340b2543d027fb4ce
LeidyRosmery <rosm3rita@hotmail.com>
2017-06-02 15:57:02 -0500

    siler izquierdo

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,20 @@ $(document).ready(function(){
   $('.after').on('click', function(){
     var imagenActual=$('.active');
     var imagenSiguiente=imagenActual.next();
+    console.log(imagenSiguiente);
     if(imagenSiguiente.length){
       imagenActual.removeClass('active').css('z-index',-10);
       imagenSiguiente.addClass('active').css('z-index',10);
+    }
+  });
+  $('.before').on('click', function(){
+    var imagenActual=$('.active');
+    var imagenAnterior=imagenActual.prev();
+        console.log(imagenAnterior);
+    if(imagenAnterior.length){
+      console.log("ingressa al if");
+      imagenActual.removeClass('active').css('z-index',-10);
+      imagenAnterior.addClass('active').css('z-index',10);
     }
   });
 });
