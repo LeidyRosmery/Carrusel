a6fc6668990c5530939add84299d27f2b4275dd3
LeidyRosmery <rosm3rita@hotmail.com>
2017-06-02 17:21:21 -0500

    slider ciclico

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('.after').on('click', function(){
-    var imagenActual=$('.active');
-    var imagenSiguiente=imagenActual.next();
-    var ultimoImagen=$('#slider img:last-child');
-    console.log(imagenActual);
-      console.log(ultimoImagen);
+    $('.after').on('click', function() {
+        var imagenActual = $('.active');
+        var imagenSiguiente = imagenActual.next();
+        var ultimoImagen = $('#slider img:last-child');
+        console.log(imagenActual);
+        console.log(ultimoImagen);
 
-  /*  if(imagenActual==ultimoImagen){
-      $('#slider img:first-child').insertBefore('#slider img:last-child');
-    }*/
-    if(imagenSiguiente.length){
-      imagenActual.removeClass('active').css('z-index',-10);
-      imagenSiguiente.addClass('active').css('z-index',10);
-    $('#slider img:last-child').insertBefore('#slider img:first-child');
-      }
-  });
-  $('.before').on('click', function(){
-    var imagenActual=$('.active');
-    var imagenAnterior=imagenActual.prev();
+        /*  if(imagenActual==ultimoImagen){
+            $('#slider img:first-child').insertBefore('#slider img:last-child');
+          }*/
+        if (imagenSiguiente.length) {
+            imagenActual.removeClass('active').css('z-index', -10);
+            imagenSiguiente.addClass('active').css('z-index', 10);
+            $('#slider img:last-child').insertBefore('#slider img:first-child');
+        }
+    });
+    $('.before').on('click', function() {
+        var imagenActual = $('.active');
+        var imagenAnterior = imagenActual.prev();
         console.log(imagenAnterior);
-    if(imagenAnterior.length){
-      console.log("ingressa al if");
-      imagenActual.removeClass('active').css('z-index',-10);
-      imagenAnterior.addClass('active').css('z-index',10);
-          $('#slider img:last-child').insertBefore('#slider img:first-child');
-    }
-  });
+        if (imagenAnterior.length) {
+            console.log("ingressa al if");
+            imagenActual.removeClass('active').css('z-index', -10);
+            imagenAnterior.addClass('active').css('z-index', 10);
+            $('#slider img:last-child').insertBefore('#slider img:first-child');
+        }
+    });
 });
