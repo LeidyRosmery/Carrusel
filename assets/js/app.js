0e3e6350d0004360c614355f3b1349df06e78213
LeidyRosmery <rosm3rita@hotmail.com>
2017-06-05 08:12:28 -0500

    controles de abajo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,41 @@
 $(document).ready(function() {
+  //funcion para cambiar imagen con controles de puntos
+      $('.controles').on('click',function(e){
+        var pos=e.target;
+        $('span').removeClass('color-activo');
+        pos.classList.add('color-activo');
+        img=$('.img-carrusel[alt='+pos.id+']');
+        img.addClass('active').css('z-index', 50);
+        console.log('span id'+ pos.id);
+        console.log('alt imagen'+ img.attr('alt'));
+      });
 
-    $('.after').on('click', function() {
+      $('.after').on('click', function() {
+        $('span').removeClass('color-activo');
         var imagenActual = $('.active');
+        var spanActual=$('#'+imagenActual.attr('alt'));
         var imagenSiguiente = imagenActual.next();
+        var spanSiguiente=spanActual.next();
         var ultimoImagen = $('#slider img:last-child');
-        console.log(imagenActual);
-        console.log(ultimoImagen);
-
-        /*  if(imagenActual==ultimoImagen){
-            $('#slider img:first-child').insertBefore('#slider img:last-child');
-          }*/
         if (imagenSiguiente.length) {
             imagenActual.removeClass('active').css('z-index', -10);
             imagenSiguiente.addClass('active').css('z-index', 10);
-            $('#slider img:first-child').insertBefore('#slider img:last-child');
-        }
+            spanActual.removeClass('color-activo');
+            spanSiguiente.addClass('color-activo');
+            $('#slider img:first-child').insertAfter('#slider img:last-child');
+            console.log(imagenActual);
+            console.log(spanActual);
+          }
+      //  if (spanActual.attr('id')==7) {
     });
     $('.before').on('click', function() {
         var imagenActual = $('.active');
         var imagenAnterior = imagenActual.prev();
-        console.log(imagenAnterior);
+        var spanActual=$('#'+imagenActual.attr('alt'));
+        var spanSiguiente=spanActual.prev();
         if (imagenAnterior.length) {
-            console.log("ingressa al if");
+            spanActual.removeClass('color-activo');
+            spanSiguiente.addClass('color-activo');
             imagenActual.removeClass('active').css('z-index', -10);
             imagenAnterior.addClass('active').css('z-index', 10);
             $('#slider img:last-child').insertBefore('#slider img:first-child');
